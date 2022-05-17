ad61963d2c63debac2df873471813c4504532655
Santiago <tiagopr1606@gmail.com>
2022-05-17 17:21:17 -0500

    50

--- controlador.js
+++ controlador.js
@@ -95,7 +95,7 @@ let producto11={
     estado:"Nuevo",
     popularidad:"75%",
     pais:"Taiwan",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2011.jpeg?alt=media&token=ff89295b-b2b1-4c91-82a1-4d7bc727ca2f"
 }
 
 let producto12={
@@ -104,7 +104,7 @@ let producto12={
     estado:"Nuevo",
     popularidad:"73%",
     pais:"Taiwan",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2012.jpg?alt=media&token=070c9b3f-5cf9-46b0-a739-9054443a13be"
 }
 
 let producto13={
@@ -133,7 +133,320 @@ let producto15={
     pais:"Taiwan",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2015.jpg?alt=media&token=7175a790-2a41-4ebf-b853-d8c0c36c2060"
 }
+let producto16={
+    nombre:"producto16",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"27%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%201.png?alt=media&token=409f122c-b800-4ed0-9eb6-33637f1fede8"
+}
 
+let producto17={
+    nombre:"producto17",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"85%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%202.jfif?alt=media&token=13104e4f-4dac-4ffc-8e49-fb2ed857f990"
+}
+
+let producto18={
+    nombre:"producto18",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"73%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%203.jfif?alt=media&token=a8fed35b-9939-4fad-9f7b-e762eaf06c59"
+}
+
+let producto19={
+    nombre:"producto19",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"54%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%204.jpg?alt=media&token=0fb59402-8fa9-4e4f-a021-4fe182eaf9b6"
+}
+
+let producto20={
+    nombre:"producto20",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"89%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%205.jpg?alt=media&token=01d5b083-4369-4606-8266-43cf4fec889e"
+}
+
+let producto21={
+    nombre:"producto21",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"68%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%206.png?alt=media&token=b19576a7-d92a-457f-9d0e-0d500ad3e1f8"
+}
+
+let producto22={
+    nombre:"producto22",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"91%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%207.jpg?alt=media&token=e75a5c78-eb8e-45e3-a339-9314c3f1bef0"
+}
+
+let producto23={
+    nombre:"producto23",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"87%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%208.jpg?alt=media&token=df16c764-7b61-4940-ae06-33c96032e14a"
+}
+
+let producto24={
+    nombre:"producto24",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"45%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%209.jpg?alt=media&token=0e40fc1e-a05a-422b-9665-5d081a14c442"
+}
+
+let producto25={
+    nombre:"producto25",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"93%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2010.jpg?alt=media&token=9977b204-4ae5-489e-9a29-ef0da7ad69f6"
+}
+
+let producto26={
+    nombre:"producto26",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"75%",
+    pais:"Taiwan",
+    foto:""
+}
+
+let producto27={
+    nombre:"producto27",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"73%",
+    pais:"Taiwan",
+    foto:""
+}
+
+let producto28={
+    nombre:"producto28",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"64%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2013.jfif?alt=media&token=113184b2-333c-48d0-a192-700e3d6d36cd"
+}
+
+let producto29={
+    nombre:"producto29",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"80%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2014.jpg?alt=media&token=e32d90f8-f9ea-408d-8e8d-08e95bc2ccb0"
+}
+
+let producto30={
+    nombre:"producto30",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"37%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2015.jpg?alt=media&token=7175a790-2a41-4ebf-b853-d8c0c36c2060"
+}
+
+let producto31={
+    nombre:"producto31",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"27%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%201.png?alt=media&token=409f122c-b800-4ed0-9eb6-33637f1fede8"
+}
+
+let producto32={
+    nombre:"producto32",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"85%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%202.jfif?alt=media&token=13104e4f-4dac-4ffc-8e49-fb2ed857f990"
+}
+
+let producto33={
+    nombre:"producto33",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"73%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%203.jfif?alt=media&token=a8fed35b-9939-4fad-9f7b-e762eaf06c59"
+}
+
+let producto34={
+    nombre:"producto34",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"54%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%204.jpg?alt=media&token=0fb59402-8fa9-4e4f-a021-4fe182eaf9b6"
+}
+
+let producto35={
+    nombre:"producto35",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"89%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%205.jpg?alt=media&token=01d5b083-4369-4606-8266-43cf4fec889e"
+}
+
+let producto36={
+    nombre:"producto36",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"68%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%206.png?alt=media&token=b19576a7-d92a-457f-9d0e-0d500ad3e1f8"
+}
+
+let producto37={
+    nombre:"producto37",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"91%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%207.jpg?alt=media&token=e75a5c78-eb8e-45e3-a339-9314c3f1bef0"
+}
+
+let producto38={
+    nombre:"producto38",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"87%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%208.jpg?alt=media&token=df16c764-7b61-4940-ae06-33c96032e14a"
+}
+
+let producto39={
+    nombre:"producto39",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"45%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%209.jpg?alt=media&token=0e40fc1e-a05a-422b-9665-5d081a14c442"
+}
+
+let producto40={
+    nombre:"producto40",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"93%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2010.jpg?alt=media&token=9977b204-4ae5-489e-9a29-ef0da7ad69f6"
+}
+
+let producto41={
+    nombre:"producto41",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"75%",
+    pais:"Taiwan",
+    foto:""
+}
+
+let producto42={
+    nombre:"producto42",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"73%",
+    pais:"Taiwan",
+    foto:""
+}
+
+let producto43={
+    nombre:"producto43",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"64%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2013.jfif?alt=media&token=113184b2-333c-48d0-a192-700e3d6d36cd"
+}
+
+let producto44={
+    nombre:"producto44",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"80%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2014.jpg?alt=media&token=e32d90f8-f9ea-408d-8e8d-08e95bc2ccb0"
+}
+
+let producto45={
+    nombre:"producto45",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"37%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2015.jpg?alt=media&token=7175a790-2a41-4ebf-b853-d8c0c36c2060"
+}
+
+let producto46={
+    nombre:"producto46",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"75%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2011.jpeg?alt=media&token=ff89295b-b2b1-4c91-82a1-4d7bc727ca2f"
+}
+
+let producto47={
+    nombre:"producto47",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"73%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2012.jpg?alt=media&token=070c9b3f-5cf9-46b0-a739-9054443a13be"
+}
+
+let producto48={
+    nombre:"producto48",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"64%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2013.jfif?alt=media&token=113184b2-333c-48d0-a192-700e3d6d36cd"
+}
+
+let producto49={
+    nombre:"producto49",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"80%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2014.jpg?alt=media&token=e32d90f8-f9ea-408d-8e8d-08e95bc2ccb0"
+}
+
+let producto50={
+    nombre:"producto50",
+    precio:500000,
+    estado:"Nuevo",
+    popularidad:"37%",
+    pais:"Taiwan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-8a55a.appspot.com/o/Foto%2015.jpg?alt=media&token=7175a790-2a41-4ebf-b853-d8c0c36c2060"
+}
 //ARREGLO
 let productos=Array(
     producto1,
@@ -150,7 +463,42 @@ let productos=Array(
     producto12,
     producto13,
     producto14,
-    producto15
+    producto15,
+    producto16,
+    producto17,
+    producto18,
+    producto19,
+    producto20,
+    producto21,
+    producto22,
+    producto23,
+    producto24,
+    producto25,
+    producto26,
+    producto27,
+    producto28,
+    producto29,
+    producto30,
+    producto31,
+    producto32,
+    producto33,
+    producto34,
+    producto35,
+    producto36,
+    producto37,
+    producto38,
+    producto39,
+    producto40,
+    producto41,
+    producto42,
+    producto43,
+    producto44,
+    producto45,
+    producto46,
+    producto47,
+    producto48,
+    producto49,
+    producto50
 )
 
 //RECORRO EL ARREGLO
